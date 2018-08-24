483105a7b500552b6c2cec7cd875e801e79d40b6
Alberto <30817348+betoal@users.noreply.github.com>
2018-08-23 19:13:49 -0500

    Create Tarea1.cpp

---
+++ Tema 1/Tarea1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdlib>
+using std::cout;
+using std::cin;
+using std::malloc;
+using std::free;
+
+typedef struct list_int {
+	int n;
+	struct list_int *ptr;
+} LISTA;
+
+LISTA *init_list, *aux, *e;
+int Elems = 1;
+
+void insertAfter(struct LISTA* prev_node, int new_data)
+{
+    if (prev_node == NULL) 
+    { 
+       printf("El nodo no puede ser NULL");       
+       return;  
+    }  
+    struct LISTA* new_node =(struct LISTA*) malloc(sizeof(LISTA));
+    new_node->n  = new_data;
+    new_node->ptr = prev_node->next; 
+    prev_node->ptr = new_node;
+}
+
+void deleteNode(struct LISTA **head_ref, int key)
+{
+    struct LISTA* temp = *head_ref, *prev;
+    if (temp != NULL && temp->data == key)
+    {
+        *head_ref = temp->next;
+        free(temp);
+        return;
+    }
+    while (temp != NULL && temp->n != key)
+    {
+        prev = temp;
+        temp = temp->ptr;
+    }
+    if (temp == NULL) return;
+    prev->ptr = temp->ptr;
+    free(temp);
+}
+
+
+int main() {
+
+
+	cout << "Dar 5 elementos ordenados a la lista\n";
+	e = (LISTA*) malloc(sizeof(LISTA));
+	cout << "Elemento " << Elems << "  ";
+	cin >> e->n;
+	e->ptr = NULL;
+	aux = e;
+	init_list = e;
+	Elems++;
+	while (Elems <= 5) {
+		e = (LISTA*) malloc(sizeof(LISTA));
+		cout << "Elemento " << Elems << "  ";
+		cin >> e->n;
+		aux->ptr = e;
+		e->ptr = NULL;
+		aux = e;
+		Elems++;
+	}
+	cout << "Los valores son: \n";
+	aux = init_list;
+	while (aux != NULL) {
+		cout << aux->n << "\n";
+		aux = aux->ptr;
+	}
+	cin.get();
+	cin.get();
+	return 0;
+}
