4f0d4679dc55511f6968dbbdf3c792d094f081d0
hca <hca@itam.mx>
2018-09-10 14:06:51 -0500

    Captura Cerámica

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -10,12 +10,12 @@ void lineSegment(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(0.4,0.6,0.2);
 	glBegin(GL_LINES);
-		glVertex3f(200,145,-1);
+		glVertex3f(20,145,-1);
 		glVertex3f(60,15,-1);
-
 	glEnd();
 	glFlush();
 }
+
 
 void main(int argc, char** argv) {
 	glutInit(&argc, argv);
